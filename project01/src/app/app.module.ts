681206b5cfad29c9dddcee76529e97a04944c14e
kranuj3122 <kumaranuj22799@gmail.com>
2022-06-30 23:20:53 +0530

    done

---
+++ project01/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NewComponentComponent } from './new-component/new-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NewComponentComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ project01/src/app/new-component/new-component.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[app-new-component]',
+  template: `
+    <h2>Hello from New Component</h2>
+    // interpolation
+    <h3>Welcome {{ name }}</h3>
+    <h3>{{ greeting() }}</h3>
+    <h3>{{ 'welcome ' + name }}</h3>
+    <h3>{{ 2 + 2 }}</h3>
+
+    // property-binding
+    <input value="Anuj" type="text" [id]="myId" />
+    <input [disabled]="false" value="kumar" type="text" id="{{ myId }}" />
+    <input bind-disabled="true" value="kumar" type="text" id="{{ myId }}" />
+
+    // class binding
+    <h2 [class]="myClass">Welcome {{ name }}</h2>
+    <h2 class="special-text" [class]="myClass">{{"02 " + name }}</h2>
+    <h2 [class.danger-text]="hasError">{{ name }}</h2>
+    <h2 [ngClass]="multipleClass">{{ name }}</h2>
+  `,
+  styles: [
+    `
+      .success-text {
+        color: green;
+      }
+      .danger-text {
+        color: red;
+      }
+      .special-text {
+        font-style: italic;
+      }
+      h3 {
+        color: blue;
+      }
+    `,
+  ],
+})
+export class NewComponentComponent implements OnInit {
+  public name = 'Anuj';
+  public myId = 'testId';
+  public myClass = 'success-text';
+  public hasError = true;
+  public isSpecial = true;
+  public multipleClass = {
+    'success-text': !this.hasError,
+    'danger-text': this.hasError,
+    'special-text': this.isSpecial,
+  };
+  public greeting() {
+    return 'Good morning ' + this.name;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
